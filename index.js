6d2823f88d6b3b53f00cfd629491d7ea3c34dc74
MivelDevelopment <milosveljkovic1992@gmail.com>
2023-01-16 15:00:50 +0100

    create signup api

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.API_PORT;
+const warehouseURL = process.env.MONGO_WAREHOUSE_URL;
+const mongoose = require('mongoose');
 
 const app = express();
-
 app.use(express.json());
+app.use(cors());
 
 app.use('/auth', require('./routes/auth'));
 
-app.listen(5000, () => {
-  console.log('Listening on port 5000');
-})
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(warehouseURL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log('Database connection failed. Server not started.');
+    console.error(err);
+  });
--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,53 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const User = require('../models/user.model');
 
-
+// SIGN UP
 router.post('/signup', [
   check('username', 'Username must be at least 3 characters long').isLength(4),
   check('email', 'Invalid email address').isEmail(),
   check('password', 'Password must be at least 6 characters long').isLength(6)
-], (req, res) => {
-  const { username, email, password } = req.body;
-  const errors = validationResult(req);
+], async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      errors: errors.array()
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      });
+    }
+
+    // Check for duplicate users
+    const usernameExists = await User.exists({ username });
+    const emailExists = await User.exists({ email: email.toLowerCase() });
+
+    if (usernameExists) return res.status(409).json({
+      errors: [{ msg: 'Username already registered' }]
+    });
+
+    if (emailExists) return res.status(409).json({
+      errors: [{ msg: 'Email already registered' }]
+    });
+
+    // Encrypt password
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    // Create user in database
+    const user = await User.create({
+      username,
+      email: email.toLowerCase(),
+      password: encryptedPassword
+    });
+
+    res.status(201).send(user);
+  } catch (error) {
+    return res.status(500).json({
+      errors: [{ msg: 'Error occured. Please try again' }]
     });
   }
-
-  res.status(200).send(req.body)
 });
 
 module.exports = router;
