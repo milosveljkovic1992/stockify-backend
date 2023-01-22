b85c98e93f5b78f24b1f4e9d221212c1e94021d9
MivelDevelopment <milosveljkovic1992@gmail.com>
2023-01-22 22:54:42 +0100

    extend user model

--- models/user.model.js
+++ models/user.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
   username: { type: String, unique: false },
   email: { type: String, unique: true },
-  password: String
+  password: String,
+  role: String
 });
 
 module.exports = mongoose.model('user', userSchema);
