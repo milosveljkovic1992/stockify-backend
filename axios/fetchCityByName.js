9965331617881920088c5d779b62412011a5cb8f
MivelDevelopment <milosveljkovic1992@gmail.com>
2023-02-13 21:30:37 +0100

    create location api for fetching cities

---
+++ axios/fetchCityByName.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+require('dotenv').config();
+const xParseApplicationId = process.env.X_PARSE_APPLICATION_ID;
+const xParseApiKey = process.env.X_PARSE_REST_API_KEY;
+
+const fetchCityByName = async (name) => {
+  const where = encodeURIComponent(
+    JSON.stringify({
+      name: {
+        $regex: `${name}`,
+      },
+    }),
+  );
+
+  try {
+    const data = await axios(
+      `https://parseapi.back4app.com/classes/Continentscountriescities_City?limit=3&where=${where}`,
+      {
+        headers: {
+          'X-Parse-Application-Id': xParseApplicationId,
+          'X-Parse-REST-API-Key': xParseApiKey,
+        }
+      }
+    );
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = fetchCityByName;
